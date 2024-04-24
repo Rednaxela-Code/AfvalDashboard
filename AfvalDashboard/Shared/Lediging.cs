fba9ce2534fc28fba1f69047043061e51c0323b8
Alexander <64273841+Snoozebugs@users.noreply.github.com>
2024-04-24 13:07:15 +0200

    Built DataSeeding functions

--- AfvalDashboard/Shared/Lediging.cs
+++ AfvalDashboard/Shared/Lediging.cs
@@ -5,5 +5,28 @@
         public DateTime Tijd { get; set; }
         public Double Gewicht { get; set; }
         public bool VerbodenLading { get; set; }
+
+        public Lediging()
+        {
+            
+        }
+
+        public List<Lediging> MaakHonderLedigingen()
+        {
+            var random = new Random();
+            var list = new List<Lediging>();
+            for (int i = 0; i < 100; i++)
+            {
+                var lediging = new Lediging()
+                {
+                    Id = i,
+                    Tijd = DateTime.Now,
+                    Gewicht = random.NextDouble() * 100,
+                    VerbodenLading = random.Next(2) == 0
+                };
+                list.Add(lediging);
+            }
+            return list;
+        }
     }
 }
--- AfvalDashboard/Shared/Wagen.cs
+++ AfvalDashboard/Shared/Wagen.cs
@@ -1,6 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace AfvalDashboard.Shared
+﻿namespace AfvalDashboard.Shared
 {
     public class Wagen : DataEntity
     {
@@ -8,5 +6,28 @@ namespace AfvalDashboard.Shared
         public bool Achterlader { get; set; }
         public bool Zijlader { get; set; }
         public string Merk { get; set; }
+
+        public Wagen(int id, string naam, bool achterlader, bool zijlader, string merk)
+        {
+            
+        }
+
+        public List<Wagen> MaakTienWagens()
+        {
+            var list = new List<Wagen>
+            {
+                new Wagen(Id = 1, Naam = "Wagen #1", Achterlader = true, Zijlader = false, Merk = "DAF"),
+                new Wagen(Id = 2, Naam = "Wagen #2", Achterlader = false, Zijlader = true, Merk = "Scania"),
+                new Wagen(Id = 3, Naam = "Wagen #3", Achterlader = true, Zijlader = false, Merk = "Mercedes"),
+                new Wagen(Id = 4, Naam = "Wagen #4", Achterlader = false, Zijlader = true, Merk = "Renault"),
+                new Wagen(Id = 5, Naam = "Wagen #5", Achterlader = true, Zijlader = false, Merk = "Volvo"),
+                new Wagen(Id = 6, Naam = "Wagen #6", Achterlader = false, Zijlader = true, Merk = "DAF"),
+                new Wagen(Id = 7, Naam = "Wagen #7", Achterlader = true, Zijlader = false, Merk = "Scania"),
+                new Wagen(Id = 8, Naam = "Wagen #8", Achterlader = false, Zijlader = true, Merk = "Mercedes"),
+                new Wagen(Id = 9, Naam = "Wagen #9", Achterlader = true, Zijlader = false, Merk = "Renault"),
+                new Wagen(Id = 10, Naam = "Wagen #10", Achterlader = false, Zijlader = true, Merk = "Volvo")
+            };
+            return list;
+        }
     }
 }
