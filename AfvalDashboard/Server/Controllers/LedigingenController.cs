8e264e200269ac6ff113bb164e307686fd503353
Alexander <64273841+Snoozebugs@users.noreply.github.com>
2024-04-24 16:09:50 +0200

    Centered Filter in FetchLedigingen.razor

--- AfvalDashboard/Server/Controllers/LedigingenController.cs
+++ AfvalDashboard/Server/Controllers/LedigingenController.cs
@@ -28,7 +28,12 @@ namespace AfvalDashboard.Server.Controllers
 
             for (int i = 1; i < 101; i++)
             {
-                var lediging = new Lediging(i, DateTime.Now.AddHours(i), random.NextDouble() * 100, random.Next(2) == 0, random.Next(1, 10).ToString());
+                var lediging = new Lediging(
+                    i,
+                    DateTime.Now.AddHours(i),
+                    random.NextDouble() * 100,
+                    random.Next(2) == 0,
+                    random.Next(1, 10).ToString());
                 list.Add(lediging);
             }
             return list;
