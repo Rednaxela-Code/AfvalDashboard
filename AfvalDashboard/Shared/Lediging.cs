5f761156bf6f71ed4875e8a58f368b01ae1603d0
Alexander <64273841+Snoozebugs@users.noreply.github.com>
2024-04-24 15:21:58 +0200

    Adding WagenNummer Property

--- AfvalDashboard/Shared/Lediging.cs
+++ AfvalDashboard/Shared/Lediging.cs
@@ -5,13 +5,15 @@
         public DateTime Tijd { get; set; }
         public Double Gewicht { get; set; }
         public bool VerbodenLading { get; set; }
+        public int WagenNummer { get; set; }
 
-        public Lediging(int id, DateTime tijd, double gewicht, bool verbodenLading)
+        public Lediging(int id, DateTime tijd, double gewicht, bool verbodenLading, int wagenNummer)
         {
             Id = id;
             Tijd = tijd;
             Gewicht = gewicht;
             VerbodenLading = verbodenLading;
+            WagenNummer = wagenNummer;
         }
     }
 }
