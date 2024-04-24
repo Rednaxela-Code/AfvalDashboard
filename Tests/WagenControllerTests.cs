709febb0af4323065e806e82bcd5baccaa7aae3e
Alexander <64273841+Snoozebugs@users.noreply.github.com>
2024-04-24 16:35:07 +0200

    Added WagenController_ReturnsTenWagenObjects test

--- Tests/WagenControllerTests.cs
+++ Tests/WagenControllerTests.cs
@@ -1,6 +1,30 @@
-﻿namespace AfvalDashboard.Tests
+﻿using AfvalDashboard.Server.Controllers;
+using AfvalDashboard.Shared;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace AfvalDashboard.Tests
 {
     public class WagenControllerTests
     {
+        private readonly Mock<ILogger<WagenController>> _mockLogger;
+        private readonly WagenController _controller;
+
+        public WagenControllerTests()
+        {
+            _mockLogger = new Mock<ILogger<WagenController>>();
+            _controller = new WagenController(_mockLogger.Object);
+        }
+
+        [Fact]
+        public void WagenController_ReturnsTenWagenObjects()
+        {
+            // Act
+            var result = _controller.Get();
+
+            // Assert
+            var wagens = Assert.IsAssignableFrom<IEnumerable<Wagen>>(result);
+            Assert.Equal(10, wagens.Count());
+        }
     }
 }
