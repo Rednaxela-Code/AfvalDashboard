b7bca217b7416e81997be2b96bdb1b7a9bfa0429
Alexander <64273841+Snoozebugs@users.noreply.github.com>
2024-04-24 14:17:19 +0200

    Basic ledigingen component done

---
+++ AfvalDashboard/Server/Controllers/LedigingenController.cs
@@ -0,0 +1,32 @@
+using AfvalDashboard.Shared;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AfvalDashboard.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LedigingenController : ControllerBase
+    {
+
+        private readonly ILogger<LedigingenController> _logger;
+
+        public LedigingenController(ILogger<LedigingenController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Lediging> Get()
+        {
+            var random = new Random();
+            var list = new List<Lediging>();
+
+            for (int i = 1; i < 101; i++)
+            {
+                var lediging = new Lediging(i, DateTime.Now.AddHours(i), random.NextDouble() * 100, random.Next(2) == 0);
+                list.Add(lediging);
+            }
+            return list;
+        }
+    }
+}
--- AfvalDashboard/Shared/Lediging.cs
+++ AfvalDashboard/Shared/Lediging.cs
@@ -6,27 +6,12 @@
         public Double Gewicht { get; set; }
         public bool VerbodenLading { get; set; }
 
-        public Lediging()
+        public Lediging(int id, DateTime tijd, double gewicht, bool verbodenLading)
         {
-            
-        }
-
-        public List<Lediging> MaakHonderLedigingen()
-        {
-            var random = new Random();
-            var list = new List<Lediging>();
-            for (int i = 0; i < 100; i++)
-            {
-                var lediging = new Lediging()
-                {
-                    Id = i,
-                    Tijd = DateTime.Now,
-                    Gewicht = random.NextDouble() * 100,
-                    VerbodenLading = random.Next(2) == 0
-                };
-                list.Add(lediging);
-            }
-            return list;
+            Id = id;
+            Tijd = tijd;
+            Gewicht = gewicht;
+            VerbodenLading = verbodenLading;
         }
     }
 }
